46918c6c66593098d9d4f25fd5aef11ad1d58469
Balu G <72133880+BaluG123@users.noreply.github.com>
2024-06-17 23:56:52 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import React from "react";
+import { Provider } from "react-redux";
+import store ,{persistor} from "./src/redux/store";
+import Navigation from './src/Navigation/navigation';
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+const App = () =>{
+  return(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      <Navigation />
+      </PersistGate>
+    </Provider>
+  )
+}
+
+export default App;
